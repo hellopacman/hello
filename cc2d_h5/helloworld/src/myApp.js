eb2712969a03494bcf810c07f3856ca23d849d90
David Wu <hellopacman@hotmail.com>
2013-03-17 23:17:40 +0800

    test

--- cc2d_h5/helloworld/src/myApp.js
+++ cc2d_h5/helloworld/src/myApp.js
@@ -43,19 +43,6 @@ var MyLayer = cc.Layer.extend({
         // ask director the window size
         var size = cc.Director.getInstance().getWinSize();
 
-        // add a "close" icon to exit the progress. it's an autorelease object
-        var closeItem = cc.MenuItemImage.create(
-            "res/CloseNormal.png",
-            "res/CloseSelected.png",
-            function () {
-                history.go(-1);
-            },this);
-        closeItem.setAnchorPoint(cc.p(0.5, 0.5));
-
-        var menu = cc.Menu.create(closeItem, null);
-        menu.setPosition(cc.PointZero());
-        this.addChild(menu, 1);
-        closeItem.setPosition(cc.p(size.width - 20, 20));
 
         /////////////////////////////
         // 3. add your codes below...
@@ -66,17 +53,16 @@ var MyLayer = cc.Layer.extend({
         this.helloLabel.setPosition(cc.p(size.width / 2, size.height - 40));
         // add the label as a child to this layer
         this.addChild(this.helloLabel, 5);
-
         var lazyLayer = new cc.LazyLayer();
         this.addChild(lazyLayer);
 
         // add "Helloworld" splash screen"
-        this.sprite = cc.Sprite.create("res/HelloWorld.png");
+        this.sprite = cc.Sprite.create(s_HelloWorld);
         this.sprite.setAnchorPoint(cc.p(0.5, 0.5));
         this.sprite.setPosition(cc.p(size.width / 2, size.height / 2));
 
         lazyLayer.addChild(this.sprite, 0);
-
+        
         return true;
     }
 
